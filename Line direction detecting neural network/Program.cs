a7d4acaa3a6ae0f6b5e074b3744a7a6d3be57949
TreacherousToast <callumoriley@gmail.com>
2017-12-09 22:03:30 -0800

    added some really important updates
    the updates make this program lighter on memory usage, and more modular and reusable for things like object detection.

--- Line direction detecting neural network/Program.cs
+++ Line direction detecting neural network/Program.cs
@@ -14,8 +14,10 @@ namespace Line_direction_detecting_neural_network
 
     class ProgramRun
     {
-        int[,,] straightLineFilters = new int[3, 3, 100];
-        int[,,] diagonalLineFilters = new int[3, 3, 100];
+        int[,,] straightLineFilters = new int[3, 3, 513];
+        int[,,] diagonalLineFilters = new int[3, 3, 513];
+        int[] straightSignificance = new int[513];
+        int[] diagonalSignificance = new int[513];
         int numOfImages = 10;
         int straightLineScore = 0;
         int diagonalLineScore = 0;
@@ -23,9 +25,14 @@ namespace Line_direction_detecting_neural_network
         
         public void setup()
         {
-            Console.WriteLine("TreacherousToast's line orientation detecting neural network v0.1");
+            Console.WriteLine("TreacherousToast's line orientation detecting neural network v0.2");
             Console.WriteLine("If you are using this algorithm, please leave a link to its GitHub page");
             Console.WriteLine();
+            for (int i = 0; i < 513; i++)
+            {
+                straightSignificance[i] = 1;
+                diagonalSignificance[i] = 1;
+            }
             for (int i = 0; i < 100; i++)
             {
                 straightLineFilters[1, 1, i] = 2;
@@ -82,20 +89,33 @@ namespace Line_direction_detecting_neural_network
                         unpackedImage[j, a] = Convert.ToInt32(currLine[j]);
                     }
                 }
+                bool filterExists = false;
                 for (int a = 1; a < 5; a++)
                 {
                     for (int b = 1; b < 5; b++)
                     {
-                        straightLineFilters[1,1, amountOfArrayWrittenTo] = unpackedImage[b, a];
-                        straightLineFilters[1,2, amountOfArrayWrittenTo] = unpackedImage[b, a + 1];
-                        straightLineFilters[1,0, amountOfArrayWrittenTo] = unpackedImage[b, a - 1];
-                        straightLineFilters[2,1, amountOfArrayWrittenTo] = unpackedImage[b + 1, a];
-                        straightLineFilters[0,1, amountOfArrayWrittenTo] = unpackedImage[b - 1, a];
-                        straightLineFilters[2,2, amountOfArrayWrittenTo] = unpackedImage[b + 1, a + 1];
-                        straightLineFilters[0,0, amountOfArrayWrittenTo] = unpackedImage[b - 1, a - 1];
-                        straightLineFilters[2,0, amountOfArrayWrittenTo] = unpackedImage[b + 1, a - 1];
-                        straightLineFilters[0,2, amountOfArrayWrittenTo] = unpackedImage[b - 1, a + 1];
-                        amountOfArrayWrittenTo++;
+                        filterExists = false;
+                        for (int c = 0; c < 513; c++)
+                        {
+                            if (straightLineFilters[1,1, c] == unpackedImage[b, a]&&straightLineFilters[1,2, c] == unpackedImage[b, a + 1]&&straightLineFilters[1,0, c] == unpackedImage[b, a - 1]&&straightLineFilters[2,1, c] == unpackedImage[b + 1, a]&&straightLineFilters[0,1, c] == unpackedImage[b - 1, a]&&straightLineFilters[2,2, c] == unpackedImage[b + 1, a + 1]&&straightLineFilters[0,0, c] == unpackedImage[b - 1, a - 1]&&straightLineFilters[2,0, c] == unpackedImage[b + 1, a - 1]&&straightLineFilters[0,2, c] == unpackedImage[b - 1, a + 1]){
+                                straightSignificance[c]++;
+                                filterExists = true;
+                                break;
+                            }
+                        }
+                        if (filterExists == false)
+                        {
+                            straightLineFilters[1,1, amountOfArrayWrittenTo] = unpackedImage[b, a];
+                            straightLineFilters[1,2, amountOfArrayWrittenTo] = unpackedImage[b, a + 1];
+                            straightLineFilters[1,0, amountOfArrayWrittenTo] = unpackedImage[b, a - 1];
+                            straightLineFilters[2,1, amountOfArrayWrittenTo] = unpackedImage[b + 1, a];
+                            straightLineFilters[0,1, amountOfArrayWrittenTo] = unpackedImage[b - 1, a];
+                            straightLineFilters[2,2, amountOfArrayWrittenTo] = unpackedImage[b + 1, a + 1];
+                            straightLineFilters[0,0, amountOfArrayWrittenTo] = unpackedImage[b - 1, a - 1];
+                            straightLineFilters[2,0, amountOfArrayWrittenTo] = unpackedImage[b + 1, a - 1];
+                            straightLineFilters[0,2, amountOfArrayWrittenTo] = unpackedImage[b - 1, a + 1];
+                            amountOfArrayWrittenTo++;
+                        }
                     }
                 }
             }
@@ -116,22 +136,36 @@ namespace Line_direction_detecting_neural_network
                         unpackedImage[j, a] = Convert.ToInt32(currLine[j]);
                     }
                 }
+                bool filterExists = false;
                 for (int a = 1; a < 5; a++)
                 {
                     for (int b = 1; b < 5; b++)
                     {
-                        diagonalLineFilters[1,1, amountOfArrayWrittenTo] = unpackedImage[b, a];
-                        diagonalLineFilters[1,2, amountOfArrayWrittenTo] = unpackedImage[b, a + 1];
-                        diagonalLineFilters[1,0, amountOfArrayWrittenTo] = unpackedImage[b, a - 1];
-                        diagonalLineFilters[2,1, amountOfArrayWrittenTo] = unpackedImage[b + 1, a];
-                        diagonalLineFilters[0,1, amountOfArrayWrittenTo] = unpackedImage[b - 1, a];
-                        diagonalLineFilters[2,2, amountOfArrayWrittenTo] = unpackedImage[b + 1, a + 1];
-                        diagonalLineFilters[0,0, amountOfArrayWrittenTo] = unpackedImage[b - 1, a - 1];
-                        diagonalLineFilters[2,0, amountOfArrayWrittenTo] = unpackedImage[b + 1, a - 1];
-                        diagonalLineFilters[0,2, amountOfArrayWrittenTo] = unpackedImage[b - 1, a + 1];
-                        amountOfArrayWrittenTo++;
+                        filterExists = false;
+                        for (int c = 0; c < 513; c++)
+                        {
+                            if (diagonalLineFilters[1,1, c] == unpackedImage[b, a]&&diagonalLineFilters[1,2, c] == unpackedImage[b, a + 1]&&diagonalLineFilters[1,0, c] == unpackedImage[b, a - 1]&&diagonalLineFilters[2,1, c] == unpackedImage[b + 1, a]&&diagonalLineFilters[0,1, c] == unpackedImage[b - 1, a]&&diagonalLineFilters[2,2, c] == unpackedImage[b + 1, a + 1]&&diagonalLineFilters[0,0, c] == unpackedImage[b - 1, a - 1]&&diagonalLineFilters[2,0, c] == unpackedImage[b + 1, a - 1]&&diagonalLineFilters[0,2, c] == unpackedImage[b - 1, a + 1]){
+                                diagonalSignificance[c]++;
+                                filterExists = true;
+                                break;
+                            }
+                        }
+                        if (filterExists == false)
+                        {
+                            diagonalLineFilters[1,1, amountOfArrayWrittenTo] = unpackedImage[b, a];
+                            diagonalLineFilters[1,2, amountOfArrayWrittenTo] = unpackedImage[b, a + 1];
+                            diagonalLineFilters[1,0, amountOfArrayWrittenTo] = unpackedImage[b, a - 1];
+                            diagonalLineFilters[2,1, amountOfArrayWrittenTo] = unpackedImage[b + 1, a];
+                            diagonalLineFilters[0,1, amountOfArrayWrittenTo] = unpackedImage[b - 1, a];
+                            diagonalLineFilters[2,2, amountOfArrayWrittenTo] = unpackedImage[b + 1, a + 1];
+                            diagonalLineFilters[0,0, amountOfArrayWrittenTo] = unpackedImage[b - 1, a - 1];
+                            diagonalLineFilters[2,0, amountOfArrayWrittenTo] = unpackedImage[b + 1, a - 1];
+                            diagonalLineFilters[0,2, amountOfArrayWrittenTo] = unpackedImage[b - 1, a + 1];
+                            amountOfArrayWrittenTo++;
+                        }
                     }
                 }
+
             }
         }        
         
@@ -151,15 +185,15 @@ namespace Line_direction_detecting_neural_network
             {
                 for (int j = 1; j < 5; j++)
                 {
-                    for (int a = 0; a < 100; a++)
+                    for (int a = 0; a < 513; a++)
                     {
                         if (unpackedImage[j, i] == straightLineFilters[1, 1, a] && unpackedImage[j, i + 1] == straightLineFilters[1, 2, a] && unpackedImage[j, i - 1] == straightLineFilters[1, 0, a] && unpackedImage[j + 1, i] == straightLineFilters[2, 1, a] && unpackedImage[j - 1, i] == straightLineFilters[0, 1, a] && unpackedImage[j + 1, i + 1] == straightLineFilters[2, 2, a] && unpackedImage[j - 1, i - 1] == straightLineFilters[0, 0, a] && unpackedImage[j + 1, i - 1] == straightLineFilters[2, 0, a] && unpackedImage[j - 1, i + 1] == straightLineFilters[0, 2, a])
                         {
-                            straightLineScore++;
+                            straightLineScore+=straightSignificance[a];
                         }
                         if (unpackedImage[j, i] == diagonalLineFilters[1, 1, a] && unpackedImage[j, i + 1] == diagonalLineFilters[1, 2, a] && unpackedImage[j, i - 1] == diagonalLineFilters[1, 0, a] && unpackedImage[j + 1, i] == diagonalLineFilters[2, 1, a] && unpackedImage[j - 1, i] == diagonalLineFilters[0, 1, a] && unpackedImage[j + 1, i + 1] == diagonalLineFilters[2, 2, a] && unpackedImage[j - 1, i - 1] == diagonalLineFilters[0, 0, a] && unpackedImage[j + 1, i - 1] == diagonalLineFilters[2, 0, a] && unpackedImage[j - 1, i + 1] == diagonalLineFilters[0, 2, a])
                         {
-                            diagonalLineScore++;
+                            diagonalLineScore+=diagonalSignificance[a];
                         }
                     }
                 }
